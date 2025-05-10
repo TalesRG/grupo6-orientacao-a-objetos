db9a71b366f43fd1ae56eaedc225c0d94b666493
TalesRG <talesrodriguesgoncalves98@gmail.com>
2025-05-10 01:20:23 -0300

    feat(docs): adiciona swagger

--- projeto-tppe-backend/src/main.ts
+++ projeto-tppe-backend/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'node:process';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  console.log(process.env.DATABASE_NAME);
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Locadora')
+    .setDescription('Documentação da API da locadora')
+    .setVersion('1.0')
+    .build();
+  console.log('Rodando na porta: ', process.env.PORT ?? 3000);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
