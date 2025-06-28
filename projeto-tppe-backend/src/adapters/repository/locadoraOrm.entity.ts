0ec2ba727d7daf9a5f4c78747325403443b65e50
TalesRG <talesrodriguesgoncalves98@gmail.com>
2025-06-28 02:04:59 -0300

    refactor : altera campos da locadora

--- projeto-tppe-backend/src/adapters/repository/locadoraOrm.entity.ts
+++ projeto-tppe-backend/src/adapters/repository/locadoraOrm.entity.ts
@@ -3,7 +3,19 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @Entity('locadoras')
 export class LocadoraOrmEntity {
   @PrimaryGeneratedColumn('uuid')
-  id: number;
+  id: string;
   @Column()
   nome: string;
+
+  @Column()
+  cnpj: string;
+
+  @Column()
+  telefone: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  endereco: string;
 }
