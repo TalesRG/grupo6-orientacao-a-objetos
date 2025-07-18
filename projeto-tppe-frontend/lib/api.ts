582a8d07e4f41fb7fb28e9ea30f55314c7f9ca87
TalesRG <talesrodriguesgoncalves98@gmail.com>
2025-07-17 21:22:29 -0300

    chore: configura variavel de ambiente

--- projeto-tppe-frontend/lib/api.ts
+++ projeto-tppe-frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3002",
+    baseURL: `${String(process.env.NEXT_PUBLIC_API_URL)}`,
 })
 
 export default api
